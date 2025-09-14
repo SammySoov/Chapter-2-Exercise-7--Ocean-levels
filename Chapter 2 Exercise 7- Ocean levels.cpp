5737d34cb7d547a3174442c7b6a9581532be5b2f
smsoo <sms28243@email.vccs.edu>
2025-09-13 21:08:48 -0400

    Add project files.

---
+++ Chapter 2 Exercise 7- Ocean levels.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	double current = 98;
+	double year5 = 5 * 1.5;
+	double year7 = 7 * 1.5;
+	double year10 = 10 * 1.5;
+
+	cout << "The number of millimeters higher than the current level that the oceans level will be in 5 years is ";
+	cout << current + year5 << endl;
+	cout << "The number of millimeters higher than the current level that the oceans level will be in 7 years is ";
+	cout << current + year7 << endl;
+	cout << "The number of millimeters higher than the current level that the oceans level will be in 10 years is ";
+	cout << current + year10 << endl;
+
+}
+
